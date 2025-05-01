43e521bc355daa46ce78d704fe7af569bfee8697
damaloonazhret <poiprizamoda@gmail.com>
2025-05-01 17:18:58 +0300

    initial project

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
-import { useState } from 'react'
-import { Button } from '@components/Button'
-import './App.css'
+import { useState } from 'react';
+import { Button } from '@components/Button';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <h1>Vite + React</h1>
       <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
+        <Button onClick={() => setCount((count) => count + 1)}>count is {count} count</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
     </div>
-  )
-}
-
-export default App
+  );
+};
