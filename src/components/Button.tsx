5d779ff4e8534cea4153609d00280e427690749f
damaloonazhret <poiprizamoda@gmail.com>
2025-05-03 19:46:11 +0300

    bad code

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -23,6 +23,14 @@ export const Button = ({ children, onClick }: TButtonProps) => {
     if (onClick) onClick();
     if (onClick) onClick();
     if (onClick) onClick();
+    if (onClick) onClick();
+    if (onClick) onClick();
+    if (onClick) onClick();
+    if (onClick) onClick();
+    if (onClick) onClick();
+    if (onClick) onClick();
+    if (onClick) onClick();
+    if (onClick) onClick();
   };
   return (
     <button className={styles.button} onClick={handleClick}>
